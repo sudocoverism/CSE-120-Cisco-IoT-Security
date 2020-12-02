b1775af4ead81d1cdf5cf0c9b3a29dfd2cee1130
teknicallyadev <70416206+teknicallyadev@users.noreply.github.com>
2020-12-01 18:48:50 -0800

    added all scripts to download

--- david.py
+++ david.py
@@ -14,7 +14,21 @@ def getDependencies(url = 'https://drive.google.com/uc?export=download&id=1l9AMb
     filename = 'dependencyInstall.sh'
     filepath = os.path.join('./scripts', filename)
     urllib.urlretrieve('https://drive.google.com/uc?export=download&id=1l9AMbUfJn7RuX3P_0MMxeeHA9G3iJwbK', filepath)
-    print('Finished Downloading')
+    filename = 'dependencyUninstall.sh'
+    filepath = os.path.join('./scripts', filename)
+    urllib.urlretrieve('https://drive.google.com/uc?export=download&id=1kNVrMK2UdWAi-eMdZpLqNi6PglcjWTUr', filepath)
+    filename = 'ipQRCode.sh'
+    filepath = os.path.join('./scripts', filename)
+    urllib.urlretrieve('https://drive.google.com/uc?export=download&id=1IPpMy5ptlpkpRGW8dorVQ-oUJuZrB9Es', filepath)
+    filename = 'publicKeyQRCode.sh'
+    filepath = os.path.join('./scripts', filename)
+    urllib.urlretrieve('https://drive.google.com/uc?export=download&id=12JkJQTZJILfdHG10bdhlEVvBOex4wuUw', filepath)
+    filename = 'wgInterfaceConfig.sh'
+    filepath = os.path.join('./scripts', filename)
+    urllib.urlretrieve('https://drive.google.com/uc?export=download&id=159pOfS93tiRksBAb_7pAcGMTazMm8Cry', filepath)
+    filename = 'wgPeerConnection.sh'
+    filepath = os.path.join('./scripts', filename)
+    urllib.urlretrieve('https://drive.google.com/uc?export=download&id=1x-tSWFdHkhM3SYHnjjHnaVejKpzrVWAV', filepath)
 
 def installDependencies():
     getDependencies()
