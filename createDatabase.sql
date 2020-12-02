533bc6df474672347513c10f4638007ea92f53b8
Raman Bakoz <33104072+FrozenTopRaman@users.noreply.github.com>
2020-12-02 15:07:59 -0800

    Database upload
    dbStart = shell script, very basic run of mainDBPrgm.py
    createDatabase.sql = test of creation of database before implementation within python program
    mainDBPrgm.py = connects to sqlite3, creates the table, populates it with information.

---
+++ createDatabase.sql
@@ -0,0 +1,5 @@
+CREATE TABLE wgPeer(
+    wg_publicKey varchar(255),
+    wg_endPoint varchar(255),
+    wg_allowedIPs varchar(255)
+);
---
+++ dbStart.sh
@@ -0,0 +1,6 @@
+#Test for creation of database within sql file.
+#sqlite3 < createDatabase.sql > outputCreate.log
+#./createDatabase.sql
+
+#Run python program that implements SQL data, creates necessary table, and stores requested values.
+./mainDBPrgm.py
