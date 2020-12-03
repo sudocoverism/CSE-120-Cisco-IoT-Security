76e8dc7ff9900e7b72a7cf82b8f8d3c08a51708d
sudocoverism <56644469+sudocoverism@users.noreply.github.com>
2020-12-02 21:55:19 -0800

    Update wgPeerConnection.sh

--- scripts/wgPeerConnection.sh
+++ scripts/wgPeerConnection.sh
@@ -5,6 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" ) >/")"
 function addPeerPubKey {
   # echo the client's public key file into the server.conf
   # echo the client's IP address file into the server.conf
+  
+  echo "[Peer]" > ~/../etc/VPN/server.conf
+  echo "PublicKey=$(cat ~/../etc/VPN/clientKey)" >> ~/../etc/VPN/server.conf
+  echo "Endpoint=$(cat ~/../etc/VPN/clientIPAddr)/24" >> ~/../etc/VPN/server.conf
+  echo "AllowedIPs = 0.0.0.0/0, ::/0" >> ~/../etc/VPN/server.conf
 }
 
 if [ -d "~/../etc/VPN" ]
