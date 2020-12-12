72ca4188e5712941ab91e9ae0232b406a4a072c6
sudocoverism <56644469+sudocoverism@users.noreply.github.com>
2020-12-12 13:51:39 -0800

    Add files via upload
    Updated server/client config scripts

---
+++ scripts/clientInterfaceConnection.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+# linux command line script to install in the etc folder of the client's root directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" ) >/")"
+
+function keyGen {
+ wg genkey | tee clientInfo/privateKey | wg pubkey > clientInfo/publicKey
+ #cat ~/../etc/VPN/publicKey | qrencode -l L -v 1 -o ~/../etc/VPN/publicKey.png ''
+}
+
+function getIP {
+ ifconfig | grep -A 1 "wlan0\|etho0\|wlp61s0" | awk '{if(NR==2) print $2}' > clientInfo/clientIPAddr
+ #cat ~/../etc/VPN/clientIPAddr | qrencode -l L -v 1 -o ~/../etc/VPN/ipAddrKey.png ''
+}
+
+function clientInterfaceConfig {
+ echo "[Interface]" > ClientInfo/client.conf
+ echo "PrivateKey=$(cat ClientInfo/privateKey)" >> ClientInfo/client.conf
+ echo "Address=$(cat ClientInfo/clientIPAddr)/24" >> ClientInfo/client.conf
+ echo "ListenPort=51820" >> ClientInfo/client.conf
+ cat ClientInfo/publicKey ClientInfo/clientIPAddr > ClientInfo/key
+ cat ClientInfo/key | qrencode -l L -v 1 -o ClientInfo/keys.png
+}
+
+if [ ! -d "clientInfo" ]
+then
+ echo "VPN directory does not exist."
+ echo "Creating VPN directory"
+ mkdir ClientInfo database
+ echo "Generating public and private keys"
+ keyGen
+ echo "Getting IP address of device"
+ getIP
+ echo "Configuring client"
+ clientConfig
+else
+ echo "VPN directory already exists."
+fi
+
+exit
---
+++ scripts/serverInterfaceConfig.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+# linux command line script to install in the etc folder of the Server's root directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" ) >/")"
+
+function keyGen {
+ wg genkey | tee ServerInfo/privateKey | wg pubkey > ServerInfo/publicKey
+ #cat ~/../etc/VPN/publicKey | qrencode -l L -v 1 -o ~/../etc/VPN/publicKey.png ''
+}
+
+function getIP {
+ ifconfig | grep -A 1 "wlan0\|etho0\|wlp61s0" | awk '{if(NR==2) print $2}' > ServerInfo/serverIPAddr
+ #cat ~/../etc/VPN/serverIPAddr | qrencode -l L -v 1 -o ~/../etc/VPN/ipAddrKey.png ''
+}
+
+function serverConfig {
+ echo "[Interface]" > ServerInfo/server.conf
+ echo "PrivateKey=$(cat ServerInfo/privateKey)" >> ServerInfo/server.conf
+ echo "Address=$(cat ServerInfo/serverIPAddr)/24" >> ServerInfo/server.conf
+ echo "ListenPort=51820" >> ServerInfo/server.conf
+ echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> ServerInfo/server.conf
+ echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> ServerInfo/server.conf
+ cat ServerInfo/publicKey ServerInfo/serverIPAddr > ServerInfo/key
+ cat ServerInfo/key | qrencode -l L -v 1 -o ServerInfo/keys.png
+}
+
+if [ ! -d "ServerInfo" ]
+then
+ echo "VPN directory does not exist."
+ echo "Creating VPN directory"
+ mkdir ServerInfo database
+ echo "Generating public and private keys"
+ keyGen
+ echo "Getting IP address of device"
+ getIP
+ echo "Configuring server"
+ serverConfig
+else
+ echo "VPN directory already exists."
+fi
+
+exit
