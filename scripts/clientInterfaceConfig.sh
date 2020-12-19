13d5518103a4f2ea86c2f45886f0770fb5d38f10
sudocoverism <56644469+sudocoverism@users.noreply.github.com>
2020-12-18 18:49:11 -0800

    Update and rename clientInterfaceConnection.sh to clientInterfaceConfig.sh

--- scripts/clientInterfaceConfig.sh
+++ scripts/clientInterfaceConfig.sh
@@ -3,26 +3,27 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" ) >/")"
 
 function keyGen {
- wg genkey | tee clientInfo/privateKey | wg pubkey > clientInfo/publicKey
+ wg genkey | tee ClientInfo/privateKey | wg pubkey > ClientInfo/publicKey
  #cat ~/../etc/VPN/publicKey | qrencode -l L -v 1 -o ~/../etc/VPN/publicKey.png ''
 }
 
 function getIP {
- ifconfig | grep -A 1 "wlan0\|etho0\|wlp61s0" | awk '{if(NR==2) print $2}' > clientInfo/clientIPAddr
+ ifconfig | grep -A 1 "wlan0\|etho0\|wlp*" | awk '{if(NR==2) print $2}' > ClientInfo/clientIPAddr
  #cat ~/../etc/VPN/clientIPAddr | qrencode -l L -v 1 -o ~/../etc/VPN/ipAddrKey.png ''
 }
 
-function clientInterfaceConfig {
+function clientConfig {
  echo "[Interface]" > ClientInfo/client.conf
  echo "PrivateKey=$(cat ClientInfo/privateKey)" >> ClientInfo/client.conf
- echo "Address=$(cat ClientInfo/clientIPAddr)/24" >> ClientInfo/client.conf
+ echo "Address=$(cat ClientInfo/clientIPAddr)" >> ClientInfo/client.conf
  echo "ListenPort=51820" >> ClientInfo/client.conf
  cat ClientInfo/publicKey ClientInfo/clientIPAddr > ClientInfo/key
  cat ClientInfo/key | qrencode -l L -v 1 -o ClientInfo/keys.png
 }
 
+echo "VPN directory does not exist."
 echo "Creating VPN directory"
-mkdir ClientInfo database
+mkdir ClientInfo
 echo "Generating public and private keys"
 keyGen
 echo "Getting IP address of device"
