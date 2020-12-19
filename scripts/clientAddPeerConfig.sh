c2406bf4d3ea44c68db635b9c133c10f8a0a06ee
sudocoverism <56644469+sudocoverism@users.noreply.github.com>
2020-12-18 18:48:17 -0800

    Update and rename clientPeerConnection.sh to clientAddPeerConfig.sh

--- scripts/clientAddPeerConfig.sh
+++ scripts/clientAddPeerConfig.sh
@@ -5,12 +5,13 @@ function addPeer {
   # echo the client's public key file into the server.conf
   # echo the client's IP address file into the server.conf
   #echo "\n" >> ServerInfo/server.conf
-  printf "\n[Peer}" >> ServerInfo
-  cat tempclient.txt >> ServerInfo/server.conf
-  printf "\nPersistentKeepalive = 25" >> ServerInfo/server.conf
+  printf "\n[Peer]" >> ClientInfo/client.conf
+  cat ClientInfo/tempclient.txt >> ClientInfo/client.conf
+  #only difference between server and client AddPeerConfig.sh
+  printf "\nPersistentKeepalive = 25" >> ClientInfo/client.conf
 }
 
-if [ -f "ServerInfo/server.conf" ]
+if [ -f "ClientInfo/client.conf" ]
 then
  echo "VPN directory found."
  echo "Adding a device as a peer to the network"
