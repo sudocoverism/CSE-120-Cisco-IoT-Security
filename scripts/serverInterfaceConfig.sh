4d0b231bd64bcb36cf8fe0095c9bbc66913fcd49
sudocoverism <56644469+sudocoverism@users.noreply.github.com>
2020-12-18 18:49:50 -0800

    Update serverInterfaceConfig.sh

--- scripts/serverInterfaceConfig.sh
+++ scripts/serverInterfaceConfig.sh
@@ -8,14 +8,14 @@ function keyGen {
 }
 
 function getIP {
- ifconfig | grep -A 1 "wlan0\|etho0\|wlp61s0" | awk '{if(NR==2) print $2}' > ServerInfo/serverIPAddr
+ ifconfig | grep -A 1 "wlan0\|etho0\|wlp*" | awk '{if(NR==2) print $2}' > ServerInfo/serverIPAddr
  #cat ~/../etc/VPN/serverIPAddr | qrencode -l L -v 1 -o ~/../etc/VPN/ipAddrKey.png ''
 }
 
 function serverConfig {
  echo "[Interface]" > ServerInfo/server.conf
  echo "PrivateKey=$(cat ServerInfo/privateKey)" >> ServerInfo/server.conf
- echo "Address=$(cat ServerInfo/serverIPAddr)/24" >> ServerInfo/server.conf
+ echo "Address=$(cat ServerInfo/serverIPAddr)" >> ServerInfo/server.conf
  echo "ListenPort=51820" >> ServerInfo/server.conf
  echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> ServerInfo/server.conf
  echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> ServerInfo/server.conf
@@ -23,13 +23,15 @@ function serverConfig {
  cat ServerInfo/key | qrencode -l L -v 1 -o ServerInfo/keys.png
 }
 
+echo "VPN directory does not exist."
 echo "Creating VPN directory"
-mkdir ServerInfo database
+mkdir ServerInfo
 echo "Generating public and private keys"
 keyGen
 echo "Getting IP address of device"
 getIP
 echo "Configuring server"
 serverConfig
+
 
 exit
